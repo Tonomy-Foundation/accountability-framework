d06d290b81fac0d7a6bbec66cdcb746cfd111289
Jack Tanner <jack@jackandtheblockstalk.com>
2020-03-07 09:59:32 +0100

    build scripts

---
+++ contracts/eosio.boot/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker-compose run eosio-cdt eosio-cpp -abigen -I /var/repo/contracts/eosio.boot/include -R resource -contract eosio.boot -o /var/repo/contracts/eosio.boot/eosio.boot.wasm /var/repo/contracts/eosio.boot/src/eosio.boot.cpp
