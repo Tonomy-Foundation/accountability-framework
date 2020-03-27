9f036cbb7403ca8c0d19192d2c5b12e2754323c9
Jack Tanner <jack@jackandtheblockstalk.com>
2020-03-27 11:39:21 +0100

    using config for keys

--- config.sh
+++ config.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+echo "Creating shared environment variables"
 export SERVER_DOMAIN="ec2-18-130-79-63.eu-west-2.compute.amazonaws.com"
 export SERVER_HTTPS_DOMAIN="du0rtkac8px3u.cloudfront.net"
+
+export KEY_EOSIO="EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
+export PKEY_EOSIO="5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+
+export KEY_JACK="EOS7Jj43XvkrBiNw8Q6zUECcGK9ktbMv8jz6Vjn1qAid93389mEgr"
+export PKEY_JACK="5K5cPZHgJaFHWt3Fy4vgzc2WfLw3cLE4E1x5c6A2kx1pjL3mEg4"
+
+export KEY_KIRSTEN="EOS57RkHk6FVCstEPJbm3ezfJ9wSGtKytvx4fZGj72qHEbHvQjy4j"
+export PKEY_KIRSTEN="5Kbbe6k8TT5ejHVvy4iiy2qQvt3MF43oMF7j6GqiQ6FHRT1fVLz"
+
+export KEY_MATEJ="EOS59cUZp45bh7hX2Q6V9ebCdBShQXmr14ANG1VZ55mESLipeCsCE"
+export PKEY_MATEJ="5JJY2YCoMRVVFrYv8SffzdjcvVneFqHWKRz2DsZRBfEZjRWNgEM"
+
+export KEY_YVO="EOS7bNT2DxZVdH2kEoZFkeuWA5KoT5koDW8aeSzczPHSF4EX8WxQs"
+export PKEY_YVO="5KHnMjLZ1jk2ScP4eGHufa8S6SJUSJWTpUqKgsGycVGxHZDCidB"
+
