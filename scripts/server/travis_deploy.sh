9d4a5a7fad319247c6b576554fbe873b2ba0504f
Jack Tanner <jack@jackandtheblockstalk.com>
2020-04-10 11:00:23 +0200

    updated string modifier

--- scripts/server/travis_deploy.sh
+++ scripts/server/travis_deploy.sh
@@ -11,16 +11,20 @@ then
     echo "Using existing key"
 else
     echo "Adding server key"
-    # Need to replace " " with new line and "_" with space to get into correct .pem format from pure string env variable
+    # Need to replace "_" with new line and "#" with space to get into correct .pem format from pure string env variable
     echo "$EC2_PEM"
-    echo "$EC2_PEM"| tr " " "\n" | tr "_" " "
-    echo "$EC2_PEM"| tr " " "\n" | tr "_" " " > ../keys/ec2.pem
+    EC2_PEM_1=`echo "$EC2_PEM"| tr "#" " "`
+    echo "$EC2_PEM_1"
+    EC2_PEM_2=`echo "$EC2_PEM_1"| tr "_" "\n"`
+    echo "$EC2_PEM_2" > ../keys/ec2.pem
     cat ../keys/ec2.pem
     chmod 400 ../keys/ec2.pem
 fi
 
-SCRIPT="cd eosio-react-app; git pull origin master; ./start.sh"
+# SCRIPT="cd eosio-react-app; git pull origin master; ./start.sh"
+SCRIPT="ls"
 echo $SCRIPT
 
 SSH_LOCATION="ubuntu@"$SERVER_DOMAIN
 ssh -o "StrictHostKeyChecking no" -i ../keys/ec2.pem $SSH_LOCATION "${SCRIPT}"
+ssh -o "StrictHostKeyChecking no" -i ../keys/ec2.pem $SSH_LOCATION
