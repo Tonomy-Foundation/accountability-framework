67a8aef34004a8a92996b25f5e504e402a80b250
gangadhar jannu <gangadhar014@gmail.com>
2020-05-05 20:23:52 +0200

    Adds account controller

---
+++ back-end/controllers/accounts.controller.js
@@ -0,0 +1,144 @@
+const db = require("../models");
+const Accounts = db.accounts;
+
+// Create and Save a new Account
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.accountName ||
+    !req.body.name ||
+    !req.body.accountType ||
+    !req.body.organizations ||
+    (req.body.organizations && req.body.organizations.some(org => !(org.accountName || org.name)))
+  ) {
+    res.status(400).send({ message: 'req body should contain all the data!' });
+    return;
+  }
+  var newAccount = new Accounts({
+    accountName: req.body.accountName,
+    name: req.body.name,
+    accountType: req.body.accountType,
+    organizations: req.body.organizations,
+  });
+
+  newAccount.save().then(success => {
+    console.log('new account created', success);
+    res.send(success);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || 'Some error occurred while creating new account.'
+    });
+  });
+};
+
+// Retrieve all Accounts from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+  Tutorial.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
+
+// Find a single Account with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + id });
+    });
+};
+
+// Update a Account by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    });
+};
+
+// Delete a Account with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else {
+        res.send({
+          message: "Tutorial was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id
+      });
+    });
+};
+
+// Delete all Accounts from the database.
+exports.deleteAll = (req, res) => {
+  Tutorial.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
+};
+
+// Find all published Accounts
+exports.findAllPublished = (req, res) => {
+  Tutorial.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
