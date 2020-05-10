a0841168d168668b3dfc72d63928959af40abac6
gangadhar jannu <gangadhar014@gmail.com>
2020-05-10 13:49:56 +0200

    Updates accouns controller

--- back-end/controllers/accounts.controller.js
+++ back-end/controllers/accounts.controller.js
@@ -36,7 +36,7 @@ exports.findAll = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
-  Tutorial.find(condition)
+  Accounts.find(condition)
     .then(data => {
       res.send(data);
     })
@@ -49,20 +49,8 @@ exports.findAll = (req, res) => {
 };
 
 // Find a single Account with an id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
-
-  Tutorial.findById(id)
-    .then(data => {
-      if (!data)
-        res.status(404).send({ message: "Not found Tutorial with id " + id });
-      else res.send(data);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ message: "Error retrieving Tutorial with id=" + id });
-    });
+exports.findOne = (condition) => {
+  return Accounts.findOne(condition);
 };
 
 // Update a Account by the id in the request
@@ -75,17 +63,17 @@ exports.update = (req, res) => {
 
   const id = req.params.id;
 
-  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+  Accounts.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
     .then(data => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot update Accounts with id=${id}. Maybe Accounts was not found!`
         });
-      } else res.send({ message: "Tutorial was updated successfully." });
+      } else res.send({ message: "Accounts was updated successfully." });
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating Accounts with id=" + id
       });
     });
 };
@@ -94,31 +82,31 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.findByIdAndRemove(id)
+  Accounts.findByIdAndRemove(id)
     .then(data => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete Accounts with id=${id}. Maybe Accounts was not found!`
         });
       } else {
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: "Accounts was deleted successfully!"
         });
       }
     })
     .catch(err => {
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete Accounts with id=" + id
       });
     });
 };
 
 // Delete all Accounts from the database.
 exports.deleteAll = (req, res) => {
-  Tutorial.deleteMany({})
+  Accounts.deleteMany({})
     .then(data => {
       res.send({
-        message: `${data.deletedCount} Tutorials were deleted successfully!`
+        message: `${data.deletedCount} Accountss were deleted successfully!`
       });
     })
     .catch(err => {
@@ -131,7 +119,7 @@ exports.deleteAll = (req, res) => {
 
 // Find all published Accounts
 exports.findAllPublished = (req, res) => {
-  Tutorial.find({ published: true })
+  Accounts.find({ published: true })
     .then(data => {
       res.send(data);
     })
